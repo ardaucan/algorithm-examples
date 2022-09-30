c975d0930fcb0f23d92293ca2ab007a2cab047fd
Arda Atakan Uçan <ardaatakanucan@gmail.com>
2022-09-30 21:57:22 +0300

    insertionSort can sort an unsorted array

--- insertion-sort/InsertionSortApp.java
+++ insertion-sort/InsertionSortApp.java
@@ -1,21 +1,22 @@
 public class InsertionSortApp {
 
     public static void main(String[] args) {
-        int[] myArr = {1,2,3,4,6,7,5};
-        insertionSort(myArr, 6);
+        int[] myArr = {3,2,5,0,9,4,1,8};
+        insertionSort(myArr, 7);
         display(myArr);
     }
 
     static void insertionSort(int[] arr, int idx){
-        int temp = arr[idx];
-        while (idx != 0){
-            if (temp < arr[idx-1]){
-                arr[idx] = arr[idx-1];
-            } else {
-                arr[idx] = temp;
-                break;
+        for (int i = 1; i <= idx; i++) {
+            
+            int j = i;
+            int key = arr[j];
+            
+            while(j > 0 && key < arr[j-1]) {
+                arr[j] = arr[j-1];
+                j--;
             }
-            idx--;
+            arr[j] = key;                        
         }
     }
 
