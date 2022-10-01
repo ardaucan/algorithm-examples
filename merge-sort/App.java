a3864761e83d1d0a2c134877443cafafa85256bd
Arda Atakan Uçan <ardaatakanucan@gmail.com>
2022-10-01 15:29:36 +0300

    changed parameter names

--- merge-sort/App.java
+++ merge-sort/App.java
@@ -1,52 +1,50 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] myArr = {7,2,3,4,0,59,6,1,5};
-        
-        // array'in sıralanacağı index aralığı girilmeli. 
+        int[] myArr = { 7, 2, 3, 4, 0, 59, 6, 1, 5 };
+
+        // array'in sıralanacağı index aralığı girilmeli.
         int first = 0;
-        int last = myArr.length-1;
+        int last = myArr.length - 1;
 
         mergeSort(myArr, first, last);
         display(myArr);
     }
 
-    static void mergeSort(int[] arr, int p, int r) {
-        if (p < r) {
-            int q = (p + r) / 2;
-            mergeSort(arr, p, q);
-            mergeSort(arr, q+1, r);
-            merge(arr, p, q, r);
+    static void mergeSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid + 1, high);
+            merge(arr, low, mid, high);
         }
     }
 
-    static void merge(int[] arr, int p, int q, int r) {
-        int n1 = q - p + 1;
-        int n2 = r - q;
-        
-        int[] leftArr = new int[n1+1];
-        int[] rightArr = new int[n2+1];
-        
+    static void merge(int[] arr, int low, int mid, int high) {
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
+
+        int[] leftArr = new int[n1 + 1];
+        int[] rightArr = new int[n2 + 1];
+
         int i, j;
 
         for (i = 0; i < n1; i++) {
-            leftArr[i] = arr[p + i];
+            leftArr[i] = arr[low + i];
         }
         for (j = 0; j < n2; j++) {
-            rightArr[j] = arr[q + j + 1];
+            rightArr[j] = arr[mid + j + 1];
         }
         leftArr[n1] = (int) Double.POSITIVE_INFINITY;
         rightArr[n2] = (int) Double.POSITIVE_INFINITY;
 
         i = 0;
         j = 0;
-        
 
-        for (int k = p; k < r + 1; k++) {
+        for (int k = low; k < high + 1; k++) {
             if (leftArr[i] <= rightArr[j]) {
                 arr[k] = leftArr[i];
                 i++;
-            }
-            else {
+            } else {
                 arr[k] = rightArr[j];
                 j++;
             }
@@ -55,6 +53,7 @@ public class App {
 
     /**
      * array'in elemanlarını yazdırır.
+     * 
      * @param arr
      */
     static void display(int[] arr) {
