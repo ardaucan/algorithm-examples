2c063b89ed93c15a8ffa72d121ea11050ede6e84
Arda Atakan Uçan <ardaatakanucan@gmail.com>
2022-10-01 16:26:54 +0300

    not important changes

--- merge-sort/App.java
+++ merge-sort/App.java
@@ -20,19 +20,19 @@ public class App {
     }
 
     static void merge(int[] arr, int low, int mid, int high) {
-        int n1 = mid - low + 1;
-        int n2 = high - mid;
+        int n1 = mid - low + 1; // left array size
+        int n2 = high - mid; // right array size
 
         int[] leftArr = new int[n1 + 1];
         int[] rightArr = new int[n2 + 1];
 
-        int i, j;
+        int i, j; // i: left array index, j: right array index
 
         for (i = 0; i < n1; i++) {
             leftArr[i] = arr[low + i];
         }
         for (j = 0; j < n2; j++) {
-            rightArr[j] = arr[mid + j + 1];
+            rightArr[j] = arr[mid + 1 + j];
         }
         leftArr[n1] = (int) Double.POSITIVE_INFINITY;
         rightArr[n2] = (int) Double.POSITIVE_INFINITY;
@@ -40,7 +40,7 @@ public class App {
         i = 0;
         j = 0;
 
-        for (int k = low; k < high + 1; k++) {
+        for (int k = low; k <= high; k++) {
             if (leftArr[i] <= rightArr[j]) {
                 arr[k] = leftArr[i];
                 i++;
